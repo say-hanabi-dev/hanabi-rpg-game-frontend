e6b70125be2cf76b197a89b76ef8bc0193e15341
lars <95558542+akuel@users.noreply.github.com>
2023-03-23 13:26:46 +0800

    3.23补充4

--- js/plugins/HGSPCore.js
+++ js/plugins/HGSPCore.js
@@ -4,32 +4,32 @@
 */
 var HGSPCore = window.HGSPCore || {};
 
-HGSPCore.level = [1];   //记得在更新的时候把这个=主角等级
 HGSPCore.point = [0];   //同上，=技能点数，然后清除技能点
 
 HGSPCore.GainSP = function(id,count){
     var actor = $gameActors.actor(id);
     var name = actor._name;
-    
-    if(!HGSPCore.point[id - 1]) HGSPCore.point[id - 1] = 0;
-    HGSPCore.point[id - 1] += count;
+    if(!actor._point) actor._point = 0;
+    actor._point += count;
 
     $gameMessage.newPage();
     $gameMessage.add(name + "获得了" + count + "个技能点！");
-
 }
 HGSPCore.GainPP = function(count){
     $gameMessage.add("获得了" + count + "个属性点！");
     $gameParty.gainItem($dataItems[9],count);  //9号道具：属性点
 }
+Game_Actor.prototype.point = function() {
+    return this._point?this._point:0;
+};
 
 HGSPCore.GameActor_displayLevelUp = Game_Actor.prototype.displayLevelUp;
 Game_Actor.prototype.displayLevelUp = function(newSkills) {
     HGSPCore.GameActor_displayLevelUp.call(this,newSkills);
     id = this._actorId;
-    if(!HGSPCore.level[id - 1]) HGSPCore.level[id - 1] = 1;
-    var count = this._level - HGSPCore.level[id - 1];
-    HGSPCore.level[id - 1] = this._level;
+    if(!actor._levelup) actor._levelup = 1;
+    var count = this._level - actor._levelup;
+    actor._levelup = this._level;
 
     HGSPCore.GainSP(id,count);
     if(id === 1) HGSPCore.GainPP(2 * count);
@@ -49,13 +49,13 @@ HGSPCore.IsLearned = function(actorid,skillid){
 HGSPCore.LearnSkill = function(actorid,skillid,count){
     var actor = $gameActors.actor(actorid);
     var name = $dataSkills[skillid].name;
-    var sp = HGSPCore.point[actorid - 1];
+    var sp = actor._point;
     
     if(count <= sp){
         actor.learnSkill(skillid);
         $gameMessage.add("习得技能「" + name + "」");
         $gameSwitches.setValue(40,0);
-        HGSPCore.point[actorid - 1] -= count;
+        actor._point -= count;
     }else{
         $gameMessage.add("你的技能点不足哦(￣y▽,￣)╭ ");
     }
