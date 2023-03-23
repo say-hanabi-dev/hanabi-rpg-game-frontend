489d212fba0421137d24c278fd06db676bf074b4
lars <95558542+akuel@users.noreply.github.com>
2023-03-23 11:45:36 +0800

    3.23更新

---
+++ js/plugins/HGMonNPCCore.js
@@ -0,0 +1,138 @@
+/*:
+ * @plugindesc 怪物与伙伴特性
+ * @author Lars
+ */
+
+var HGMonNPCCore = window.HGMonNPCCore || {} ;
+
+HGMonNPCCore.CreateActor = function(id,classid){
+    if(!$dataActors[id]){
+        var list1 = ["Actor1_4","Actor3_8","Actor2_4"]; //打架图
+        var list2 = [3,7,3];                            //行走图的序号
+        var list3 = ["Actor1","Actor3","Actor2"];       //行走图
+        var list4 = [3,7,3];                            //脸的序号
+        var list5 = ["Actors1","Actor3","Actor2"];      //脸
+        var list6 = ["混子","魔法学徒","辅助法师"];       //名字
+        
+        $dataActors[id] = {
+            "id":id,
+            "battlerName":list1[classid - 2],
+            "characterIndex":list2[classid - 2],
+            "characterName":list3[classid - 2],
+            "classId":classid,
+            "equips":[0,0,0,0,0],
+            "faceIndex":list4[classid - 2],
+            "faceName":list5[classid - 2],
+            "traits":[],
+            "initialLevel":1,
+            "maxLevel":80,
+            "name":list6[classid - 2],
+            "nickname":"",
+            "note":"",
+            "profile":"",
+            "next":{}
+        };
+        $gameActors.actor(id - 1)._next = $dataActors[id];
+        DataManager.processSCDNotetags2($dataActors);
+        DataManager.processSCDNotetags3($dataActors);
+    }
+}
+
+HGMonNPCCore.AddActor = function(id){
+    $gameParty.addActor(id);
+    if(!HGSPCore.level[id]) HGSPCore.level[id] = 1;
+    if(!HGSPCore.point[id]) HGSPCore.point[id] = 0;
+}
+HGMonNPCCore.RemoveActor = function(id){
+    $gameParty.removeActor(id);
+}
+
+HGMonNPCCore.jicunActor = function(){
+    var party = $gameParty.members();
+    party = party.filter(function(member){return member.actorId() != 1});
+    
+    var choice = party.map(function(member){return member.name()}).concat("取消");
+    $gameMessage.add("要寄存哪一位伙伴呢？");
+    $gameMessage.setChoices(choice,0,choice.length);
+    $gameMessage.setChoiceCallback(function(x){
+        if(x >= choice.length - 1) return;
+        HGMonNPCCore.RemoveActor(party[x]._actorId);
+    });
+}
+HGMonNPCCore.zhaohuiActor = function(){
+    var actors = $gameActors._data.filter(function(member){return !$gameParty.members().contains(member) && member._classId >= 2 && member._classId <= 4});
+    var choice = actors.map(function(member){return member.name()}).concat("取消");
+    $gameMessage.add("要召回哪一位伙伴呢？");
+    $gameMessage.add("需要1w金币才能抵消他们被你寄存时的寂寞哦~");
+    $gameMessage.add("（或者他们抛弃你时的不要脸）");
+    $gameMessage.setChoices(choice,0,choice.length);
+    $gameMessage.setChoiceCallback(function(x){
+        if(x >= choice.length - 1) return;
+        HGMonNPCCore.AddActor(actors[x]._actorId);
+    });
+}
+
+HGMonNPCCore.Game_Actor_setup = Game_Actor.prototype.setup;
+Game_Actor.prototype.setup = function(actorId){
+    HGMonNPCCore.Game_Actor_setup.call(this,actorId);
+    this._next = $dataActors[actorId].next;
+}
+
+Game_Actor.prototype.next = function(){
+    return this._next;
+}
+
+HGMonNPCCore.RunMember = [];
+HGMonNPCCore._GameBattler_gainHp = Game_Battler.prototype.gainHp;
+Game_Battler.prototype.gainHp = function(value){
+    
+    if($gameMap.mapId() === 32){
+        if(this.name() != "赌场老千"){
+            if($gameSwitches.value(52)){//老千
+                if($gameSwitches.value(51)){//猜奇数
+                    value = value - (-value % 2) - 2;
+                    $gameSwitches.setValue(53,1);
+                }else{//猜偶数
+                    value = value - (-value % 2) - 1;
+                    $gameSwitches.setValue(53,1);
+                }
+            }else{//正常
+                $gameSwitches.setValue(53,(-value % 2 === 1) === !$gameSwitches.value(51));
+                
+            }
+        }
+    }
+    
+    if(this.name() === "地狱三头犬"){
+        if(!$gameSwitches.value(42) || !$gameSwitches.value(43) || !$gameSwitches.value(44)){   //破坏三座神像，否则无敌
+            value = 0;
+            $gameSwitches.setValue(20,1);
+        }
+    }
+    HGMonNPCCore._GameBattler_gainHp.call(this, value);
+
+    if(this.isActor()){
+        if(this._classId === 4){
+            var per = 0.2;
+            if(this._level >= 30) per = 0.25;
+            if(this._level >= 50) per = 0.3;
+            if(this.hp < this.mhp * per){
+                $gameMessage.add("有个家伙害怕了，她跑啦！！！");
+                this.hide();
+                HGMonNPCCore.RunMember.push(this._actorId);
+            }
+        }
+    }
+}
+
+
+HGMonNPCCore.DataManager_loadDataFile = DataManager.loadDataFile;
+DataManager.loadDataFile = function(name,src){
+    HGMonNPCCore.DataManager_loadDataFile.call(this,name,src);
+    if(name = '$dataMap'){
+        for(var i = 0;i < HGMonNPCCore.RunMember.length; i++)
+            $gameParty.removeActor(HGMonNPCCore.RunMember[i]);
+        HGMonNPCCore.RunMember = [];
+    }
+}
+
---
+++ js/plugins/HGSPCore.js
@@ -0,0 +1,152 @@
+/*:
+* @plugindesc 技能点与技能相关
+* @author Lars
+*/
+var HGSPCore = window.HGSPCore || {};
+
+HGSPCore.level = [1];   //记得在更新的时候把这个=主角等级
+HGSPCore.point = [0];   //同上，=技能点数，然后清除技能点
+
+HGSPCore.GainSP = function(id,count){
+    var actor = $gameActors.actor(id);
+    var name = actor._name;
+    
+    if(!HGSPCore.point[id - 1]) HGSPCore.point[id - 1] = 0;
+    HGSPCore.point[id - 1] += count;
+
+    $gameMessage.newPage();
+    $gameMessage.add(name + "获得了" + count + "个技能点！");
+
+}
+HGSPCore.GainPP = function(count){
+    $gameMessage.add("获得了" + count + "个属性点！");
+    $gameParty.gainItem($dataItems[9],count);  //9号道具：属性点
+}
+
+HGSPCore.GameActor_displayLevelUp = Game_Actor.prototype.displayLevelUp;
+Game_Actor.prototype.displayLevelUp = function(newSkills) {
+    HGSPCore.GameActor_displayLevelUp.call(this,newSkills);
+    id = this._actorId;
+    if(!HGSPCore.level[id - 1]) HGSPCore.level[id - 1] = 1;
+    var count = this._level - HGSPCore.level[id - 1];
+    HGSPCore.level[id - 1] = this._level;
+
+    HGSPCore.GainSP(id,count);
+    if(id === 1) HGSPCore.GainPP(2 * count);
+}
+
+HGSPCore.IsLearned = function(actorid,skillid){
+    var actor = $gameActors.actor(actorid);
+    var name = $dataSkills[skillid].name;
+    if(actor.isLearnedSkill(skillid)){
+        $gameMessage.newPage();
+        $gameMessage.add("您已学会「" + name + "」哦（＾▽＾）");
+        return true;
+    }
+    return false;
+}
+
+HGSPCore.LearnSkill = function(actorid,skillid,count){
+    var actor = $gameActors.actor(actorid);
+    var name = $dataSkills[skillid].name;
+    var sp = HGSPCore.point[actorid - 1];
+    
+    if(count <= sp){
+        actor.learnSkill(skillid);
+        $gameMessage.add("习得技能「" + name + "」");
+        $gameSwitches.setValue(40,0);
+        HGSPCore.point[actorid - 1] -= count;
+    }else{
+        $gameMessage.add("你的技能点不足哦(￣y▽,￣)╭ ");
+    }
+}
+
+HGSPCore.ChangeTarget = function(){
+    var member = $gameParty.members();
+    var choice = [];
+    var list = [];
+    for(var i = 0; i < member.length; i++)
+        if(member[i]._actorId != 1){
+            choice.push(member[i]._name);
+            list.push(member[i]._actorId);
+        }
+    choice.push("取消");
+    
+    $gameMessage.add("当前正在由 " + $gameActors.actor($gameVariables.value(70))._name + " 学习技能");
+    $gameMessage.add("要改由哪个角色学习技能呢？");
+    $gameMessage.setChoices(choice, choice.length, 0)
+    $gameMessage.setChoiceCallback(function(x){
+        if(x < choice.length - 1) $gameVariables.setValue(70,list[x]);
+    })
+}
+
+HGSPCore.skill = {"id":0,"cd":0,"need":0,"formula":""};
+HGSPCore.ShowSkills = function(){
+    var id = HGSPCore.skill.id;
+    var name = $dataSkills[id].name;
+    var cd = HGSPCore.skill.cd;
+    var need = HGSPCore.skill.need;
+    var des = $dataSkills[id].description;
+    var formula = HGSPCore.skill.formula;
+    
+    $gameMessage.newPage();
+    $gameMessage.add("\\n<" + name + ">CD:" + cd + "   需要技能点：" + need);
+    if(formula != "")$gameMessage.add(formula);
+    $gameMessage.add(des);
+}
+
+HGSPCore.ShowSkillsEX = function(skillname){
+    var id = HGSPCore.skill.id;
+    var name = $dataSkills[id].name;
+    var cd = HGSPCore.skill.cd;
+    var need = HGSPCore.skill.need;
+    var des = $dataSkills[id].description;
+    var formula = HGSPCore.skill.formula;
+    
+    $gameMessage.newPage();
+    $gameMessage.add("\\n<" + name + ">CD:" + cd + "   需要技能点：" + need + "   需要任意伙伴学会" + skillname);
+    if(formula != "")$gameMessage.add(formula);
+    $gameMessage.add(des);
+}
+
+
+
+HGSPCore.lightline2 = function(list1,list2){
+
+    if($gameVariables.value(70) === 0) $gameVariables.setValue(70,1);
+    for(var i = 0; i < $gameParty.members().length; i++){
+        var actor = $gameParty.members()[i];
+        
+        for(var j = 0; j < list1.length; j++){
+            
+            if(list1[j].count){
+                var need = list1[j].count;
+                for(var k = 0; k < list1[j].need.length; k++){
+                    if(actor.isLearnedSkill(list1[j].need[k])){
+                        need--;
+                        if(need === 0){
+                            $gameSwitches.setValue(21 + j, 1);
+                            break;
+                        }
+                    }
+                }
+            }else{
+                if(actor.isLearnedSkill(list1[j])){
+                    $gameSwitches.setValue(21 + j, 1);
+                }
+            }
+        }
+    }
+    for (var i = 0; i < list2.length; i++){
+        var actor = $gameActors.actor(1);
+        if(actor.isLearnedSkill(list2[i])){
+            $gameSwitches.setValue(21 + list1.length + i, 1);
+        }
+    }
+}
+
+
+
+
+
+
